95de4ade45c11ac9ee16839a5eb768aa04a4b2da
Cronje van Heerdnen <cronje.vanheerden@rapiddeploy.com>
2022-08-07 17:12:24 +0200

    Stuck with viper

--- cmd/rd/root.go
+++ cmd/rd/root.go
@@ -1,60 +1,41 @@
 package rd
 
 import (
- "fmt"
- "os"
+	"fmt"
+	"os"
 
- "github.com/cronjevh/rd-go-cli/cmd/rd/incubator"
- "github.com/spf13/cobra"
- "github.com/spf13/viper"
+	"github.com/cronjevh/rd-go-cli/cmd/rd/incubator"
+	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
 )
 
 var version = "0.0.1"
 var cfgFile string
 
 var rootCmd = &cobra.Command{
-    Use:  "rd",
-    Version: version,
-    Short: "rd - a simple CLI to transform and inspect strings",
-    Long: `rd is a super fancy CLI (kidding)
+	Use:     "rd",
+	Version: version,
+	Short:   "rd - a simple CLI to transform and inspect strings",
+	Long: `rd is a super fancy CLI (kidding)
    
 One can use rd to modify or inspect strings straight from the terminal`,
-    Run: func(cmd *cobra.Command, args []string) {
+	Run: func(cmd *cobra.Command, args []string) {
 
-    },
+	},
 }
 
 func Execute() {
-    if err := rootCmd.Execute(); err != nil {
-        fmt.Fprintf(os.Stderr, "Whoops. There was an error while executing your CLI '%s'", err)
-        os.Exit(1)
-    }
-}
-
-func init() {
-	cobra.OnInitialize(initConfig)
-
-	// setDefaults()
-
-	fmt.Println("name:", viper.Get("name"))
-	// Here you will define your flags and configuration settings.
-	// Cobra supports persistent flags, which, if defined here,
-	// will be global for your application.
-	// Add my subcommand palette
-	// rootCmd.AddCommand(info.InfoCmd)
-	rootCmd.AddCommand(incubator.IncubatorCmd)
-	rootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is $HOME/.toolbox.yaml)")
-
-	// Cobra also supports local flags, which will only run
-	// when this action is called directly.
-	rootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+	if err := rootCmd.Execute(); err != nil {
+		fmt.Fprintf(os.Stderr, "Whoops. There was an error while executing your CLI '%s'", err)
+		os.Exit(1)
+	}
 }
 
 // initConfig reads in config file and ENV variables if set.
 func initConfig() {
 	if cfgFile != "" {
 		// Use config file from the flag.
-		viper.SetConfigFile(".env")
+		viper.SetConfigFile(cfgFile)
 	} else {
 		// Find home directory.
 		home, err := os.UserHomeDir()
@@ -71,5 +52,26 @@ func initConfig() {
 	// If a config file is found, read it in.
 	if err := viper.ReadInConfig(); err == nil {
 		fmt.Fprintln(os.Stderr, "Using config file:", viper.ConfigFileUsed())
+		fmt.Println("name:", viper.Get("username"))
 	}
+}
+
+func init() {
+	cobra.OnInitialize(initConfig)
+
+	// setDefaults()
+
+	// Here you will define your flags and configuration settings.
+	// Cobra supports persistent flags, which, if defined here,
+	// will be global for your application.
+	// Add my subcommand palette
+	// rootCmd.AddCommand(info.InfoCmd)
+	rootCmd.AddCommand(incubator.IncubatorCmd)
+	rootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is $HOME/.toolbox.yaml)")
+
+	fmt.Println("name:", viper.Get("username"))
+
+	// Cobra also supports local flags, which will only run
+	// when this action is called directly.
+	rootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
 }
